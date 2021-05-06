e655f1285d3218ee4b4f61cd2e26be48dc647a41
Eduard Maghakyan <edi.maghakyan@gmail.com>
2021-05-07 01:02:11 +0200

    Added sample server

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/EduardMaghakyan/game-server
+
+go 1.16
---
+++ server_test.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+type StubPlayerStore struct {
+	scores map[string]int
+}
+
+func (s *StubPlayerStore) GetPlayerScore(name string) int {
+	score := s.scores[name]
+	return score
+}
+
+func TestGetPlayers(t *testing.T) {
+	store := StubPlayerStore{
+		map[string]int{
+			"Pepper": 20,
+			"Floyd":  10,
+		},
+	}
+	server := &PlayerServer{&store}
+
+	t.Run("return Pepper's sscore", func(t *testing.T) {
+		request := newGetScoreRequest("Pepper")
+		response := httptest.NewRecorder()
+
+		server.ServeHTTP(response, request)
+
+		assertResponseBody(t, response.Body.String(), "20")
+		assertResponseCodes(t, response.Code, http.StatusOK)
+	})
+
+	t.Run("return Floyd's score", func(t *testing.T) {
+		request := newGetScoreRequest("Floyd")
+		response := httptest.NewRecorder()
+
+		server.ServeHTTP(response, request)
+
+		assertResponseBody(t, response.Body.String(), "10")
+		assertResponseCodes(t, response.Code, http.StatusOK)
+	})
+
+	t.Run("return 404 for missing player", func(t *testing.T) {
+		request := newGetScoreRequest("Apollo")
+		response := httptest.NewRecorder()
+
+		server.ServeHTTP(response, request)
+
+		assertResponseCodes(t, response.Code, http.StatusNotFound)
+	})
+}
+
+func newGetScoreRequest(name string) *http.Request {
+	req, _ := http.NewRequest(http.MethodGet, fmt.Sprintf("/players/%s", name), nil)
+	return req
+}
+
+func assertResponseBody(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("response body is wrong, got %q, want %q", got, want)
+	}
+}
+
+func assertResponseCodes(t testing.TB, got, want int) {
+	t.Helper()
+	if got != want {
+		t.Errorf("response code is wrong, got %d, want %d", got, want)
+	}
+}
